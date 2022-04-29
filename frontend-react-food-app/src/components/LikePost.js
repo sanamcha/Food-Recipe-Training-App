420ab9e4c37bc06189fd88f219dd026c71eb32d5
sanamcha <mhr.sanam@gmail.com>
2022-04-29 12:30:44 -0700

    added like page for meal

---
+++ frontend-react-food-app/src/components/LikePost.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import UserContext from "../users/UserContext";
+import likedImg from "./img/thumb-up.png";
+import likeImg from "./img/like.png";
+
+
+const LikePost =({ m }) => {
+    const { hasLiked, likes } = useContext(UserContext);
+    const [liked, setLiked] = useState();
+    const {id} = useParams();
+    useEffect( async function updateLikedStatus(){
+        console.debug("updateLikedStatus", "id=", id)
+        
+        setLiked(hasLiked(id));
+        
+       }, [id, hasLiked]);
+    
+       //click on like btn
+       async function handleLike(e){
+           if(hasLiked(id)) return;
+           likes(id);
+           setLiked(true);
+    
+       }
+
+    return (
+        <div>{liked}
+
+            <button
+               className="btn btn-info "
+               onClick={handleLike}
+               disabled={liked} 
+               >
+               {liked ? <img src={likedImg} 
+                     alt ="liked thumb" /> 
+                     : <img src={likeImg} 
+                     alt ="like thumb" />
+                   }
+            </button>
+        </div>
+    )
+
+
+
+}
+
+export default LikePost;
