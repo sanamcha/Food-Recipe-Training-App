1fd23cf816314ba6ed9b67a4e26991b549cb24cc
sanamcha <mhr.sanam@gmail.com>
2022-04-29 12:29:51 -0700

    modified components for post meal-list

--- frontend-react-food-app/src/components/PostList.js
+++ frontend-react-food-app/src/components/PostList.js
@@ -1,54 +1,36 @@
 import React, { useState, useEffect, useContext } from "react";
-import RequestApi from "../api/RequestApi";
-import PostCardList from "./PostCardList";
 import { Link, useParams } from "react-router-dom";
-import UserContext from "../users/UserContext";
 import { ListGroupItem } from "reactstrap";
-import likedImg from "./img/thumb-up.png";
-import likeImg from "./img/like.png";
-
-// import idgen from "react-materialize/lib/idgen";
-
+import axios from "axios";
+import EditPost from "./EditPost";
+// import LikePost from "./LikePost";
 
 
 //show list of meals from api
-function PostList({  meals }) {
+function PostList({  meals, setMeals }) {
   const { id } = useParams()
-
-  // const { hasLikedToMeal, likeToMeal } = useContext(UserContext);
-  //   const [liked, setLiked] = useState();
-
-  //   useEffect( async function updateLikedStatus(){
-  //       console.debug("updateLikedStatus", "id=", id)
-        
-  //       // setLiked(hasLikedToMeal(id));
-        
-  //      }, []);
-    
-  //      //click on like btn
-  //      async function handleLike(e){
-  //          if(hasLikedToMeal(id)) return;
-  //          likeToMeal(id);
-  //          setLiked(true);
-  //      }
 
   if (!meals) return <h1>Loading....</h1>
 
-// return (
-//   <div>
-//     {meals.length 
-//     ? <PostCardList  meals = {meals} />
-//     : <p>Sorry, No results were found</p>
-//   }
-//   </div>
-// )
+
+const deleteBtn = async (id) => {
+  try{
+    const deleteBtn = await axios.delete(`http://localhost:3002/meals/${id}`, {
+      method:"DELETE"
+    });
+    setMeals(meals.filter(m => m.id !== id))
+    console.log(deleteBtn)
+    
+  }catch(err){
+    console.log(err.message)
+  }
+}
 
     return (
     
     <div>
-      {/* {liked} */}
    
-    {meals.map(m =>(
+      {meals.map(m =>(
       <div>
         <Link to={`/meals/${m.id}`} key={m.id} >
          <ListGroupItem>
@@ -61,19 +43,17 @@ function PostList({  meals }) {
      
         {/* <Link to={`/reviews`} key={m.id}>View Reviews</Link> */}
 
-        <Link to={`meals/${m.id}/add`} key={m.id}><button className="btn btn-info">Add Reviews</button>  </Link> 
 
-        {/* <button
-               className="btn btn-info "
-               onClick={handleLike}
-               disabled={liked} 
-               >
-               {liked ? <img src={likedImg} 
-                     alt ="liked thumb" /> 
-                     : <img src={likeImg} 
-                     alt ="like thumb" />
-                   }
-               </button> */}
+        {/* <LikePost m={m} /> */}
+        <Link to={`meals/${m.id}/add`} key={m.id}><button className="btn btn-info">Add Reviews</button>  </Link> 
+        
+        
+        <EditPost m={m} key={m.id}/>
+
+        <button className="btn btn-danger"
+                onClick={() => deleteBtn(m.id)}>Delete</button>
+
+
       </div>
     ))}
     
